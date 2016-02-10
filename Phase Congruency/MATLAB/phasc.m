ded6798ba8a80c76ffd79785e98d3112e420cd43
ProSlapShot <reece.nj.williams@live.com>
2016-02-10 10:10:32 +0000

    Canny code not working | Modified phasc to show phasecong3 & phasecongmono in one figure

--- Phase Congruency/MATLAB/phasc.m
+++ Phase Congruency/MATLAB/phasc.m
@@ -1,12 +1,19 @@
-im = imread('lena.tif');
-subplot(1,2,1),subimage(im)
+clear all;
+im = imread('Nix.jpg');
+subplot(3,3,2),subimage(im)
 tic
-[PC, or] = phasecongmono(im,3,10,2.1,0.55);
+[PC, or] = phasecongmono(im,3,5,2.1,0.55);
 nm = nonmaxsup(PC, or, 1.5);   % nonmaxima suppression
-bw1 = hysthresh(nm, 0.1, 0.3);  % hysteresis thresholding 0.1 - 0.3
-[PC, or] = phasecongmono(im,3,3,2.1,0.55);
-nm = nonmaxsup(PC, or, 1.5);   % nonmaxima suppression
-bw2 = hysthresh(nm, 0.1, 0.3);  % hysteresis thresholding 0.1 - 0.3
+bw = hysthresh(nm, 0.4, 0.2);  % hysteresis thresholding 0.1 - 0.3
 toc
-b1=bw1+bw2;
-subplot(1,2,2), subimage(bw)
+tic
+[PC2,m,or2] = phasecong3(im,3,6,5);
+nm2 = nonmaxsup(PC2, or2, 1.5);   % nonmaxima suppression
+bw2 = hysthresh(nm2, 0.2, 0.3);  % hysteresis thresholding 0.1 - 0.3
+toc
+subplot(3,3,4), subimage(PC)
+subplot(3,3,5), subimage(nm)
+subplot(3,3,6), subimage(bw)
+subplot(3,3,7), subimage(PC2)
+subplot(3,3,8), subimage(nm2)
+subplot(3,3,9), subimage(bw2)
